7fbe54320f87e394c8046b6cc7ae3552f8c94554
Nicolas Taffoureau <37969996+ntaff@users.noreply.github.com>
2019-02-22 08:43:41 +0100

    Add unification algorithm

--- Unification_Inference.ml
+++ Unification_Inference.ml
@@ -103,3 +103,72 @@ let nouvelleVarTp (compteur : int) (s : string option) = match s with
 	(* TESTS *)
 	nouvelleVarTp 3 None;; (* ----> - : string * int = ("a3", 4) *)
 	nouvelleVarTp 3 (Some "v");; (* ----> - : string * int = ("v3", 4) *)
+	
+(* ----------------- Inférence de types ---------------- *)
+
+(* Génération des contraintes *)
+(* val genereContraintes : int -> envt -> expr -> tp * 'a list * int = <fun> *)
+let rec genereContraintes (compteur : int) (env : envt) (exp : expr) = match exp with
+	(Const c) -> (tpConstante(c), [] , compteur)
+	| (Var v) -> ((tpVariable env v), [] , compteur)
+	| (Func (s,e)) -> let var = (nouvelleVarTp compteur (Some s)) in
+							let couple = (s, (VarT(fst(var)))) in
+								let (tpe, contrainteE, compteurE) = (genereContraintes (snd(var)) (couple::env) e) in
+									((FunT((VarT(fst(var))),tpe)), contrainteE , compteurE)
+	| (App (e1,e2)) -> let (tpe1,ce1,cpte1) = genereContraintes (compteur+1) env e1 and 
+							 (tpe2, ce2, cpte2) = genereContraintes (compteur+1) env e2 in
+								(FunT(tpe1,tpe2), ce2, cpte2);;
+
+
+
+(* Unification *)
+let rec union = fun s1 -> fun s2 ->
+  match s1 with
+      [] -> s2
+    | e::s1' -> 
+      if List.mem e s2 
+      then (union s1' s2)
+      else e:: (union s1' s2) ;;
+
+let rec fv = function
+  | ConstT c -> []
+  | VarT v -> [v]
+  | FunT (t1, t2) -> union (fv t1) (fv t2) ;;
+
+let rec applique_substitution t li = match t with
+    | ConstT c -> ConstT c
+    | VarT v -> 
+	(match cherche v li with
+	     None -> VarT v
+	   | Some trm' -> trm')
+    | FunT (t1, t2) -> FunT (applique_substitution t1 li, applique_substitution t2 li) ;;
+
+let comp_subst sbst' sbst = 
+  (List.map (fun (v, t) -> (v, applique_substitution t sbst')) sbst) @ sbst' ;;
+
+exception UnifError of string ;;
+
+let rec unif = function
+    ([], sigma) -> sigma
+  | ((ConstT c1, ConstT c2) :: e, sigma) -> if c1 = c2 then unif (e, sigma) 
+													  else raise (UnifError("clash"))
+  | ((VarT x1, VarT x2) :: e, sigma) -> if x1 = x2 then unif (e, sigma) 
+												   else let sigma' = [(x1, VarT x2)] in 
+														let f = fun (s1,s2) -> (applique_substitution s1 sigma', applique_substitution s2 sigma') in
+														let e' = List.map f e in 
+													unif (e', comp_subst sigma' sigma)
+													
+  | ((FunT (s1, s2), FunT (t1, t2)):: e, sigma) ->  unif ((s1, t1) :: (s2, t2) :: e, sigma)
+  
+  | ((VarT x, t) :: e, sigma) ->  if List.mem x (fv t) then raise (UnifError("check"))
+													   else let sigma' = [(x, t)] in 
+															let f = fun (s1,s2) -> (applique_substitution s1 sigma', applique_substitution s2 sigma') in
+															let e' = List.map f e in
+													unif (e', comp_subst sigma' sigma)
+  | ((t, VarT x) :: e, sigma) -> unif ((VarT x, t) :: e, sigma)
+  | ((_, _) :: e, sigma) -> raise (UnifError("fail")) ;;
+
+ (* Inférence de types *)
+ 
+let rec infereType (e : envt) (exp : expr) = match exp with
+	expr -> unif(genereContraintes(1,e,exp));;
